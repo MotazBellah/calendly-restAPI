05fd6fb3935da8dfcb269facd96edec82d350c0f
MotazBellah <engineer.android@yahoo.com>
2022-05-03 00:50:16 +0200

    Uncomment test cases for post

--- test/api/post.js
+++ test/api/post.js
@@ -20,35 +20,35 @@ describe("POST /api/schedule_meeting", () => {
     })
 
 
-    // it("OK, creating a new meeting", (done) => {
-    //     request(app).post("/api/schedule_meeting")
-    //         .send({ "title": "test", "meetingCreator": "Moataz", "meetingWith": "Ahmed",
-    //             "slotDate": "2022-05-04", "slotTime": "23:05", "duration": "30" })
-    //         .then((res) => {
-    //             const body = res.body;
-    //             console.log(body)
-    //             expect(body).to.contain.property("_id");
-    //             expect(body).to.contain.property("title");
-    //             expect(body).to.contain.property("slotDate");
-    //             expect(body).to.contain.property("slotTime");
-    //             done();
-    //         })
-    //         .catch((err) => done(err));
-    // });
+    it("OK, creating a new meeting", (done) => {
+        request(app).post("/api/schedule_meeting")
+            .send({ "title": "test", "meetingCreator": "Moataz", "meetingWith": "Ahmed",
+                "slotDate": "2022-05-04", "slotTime": "23:05", "duration": "30" })
+            .then((res) => {
+                const body = res.body;
+                console.log(body)
+                expect(body).to.contain.property("_id");
+                expect(body).to.contain.property("title");
+                expect(body).to.contain.property("slotDate");
+                expect(body).to.contain.property("slotTime");
+                done();
+            })
+            .catch((err) => done(err));
+    });
 
-    // it("Fail, creating a new meeting, overlaps with anthor meeting", (done) => {
-    //     request(app).post("/api/schedule_meeting")
-    //         .send({ "title": "test", "meetingCreator": "Moataz", "meetingWith": "Ahmed",
-    //             "slotDate": "2022-05-03", "slotTime": "19:05", "duration": "30" })
-    //         .then((res) => {
-    //             const body = res.body;
-    //             console.log(body)
-    //             expect(body).to.contain.property("error");
-    //             expect(body.error).to.equal('Ahmed is busy, Please select different time slot');
+    it("Fail, creating a new meeting, overlaps with anthor meeting", (done) => {
+        request(app).post("/api/schedule_meeting")
+            .send({ "title": "test", "meetingCreator": "Moataz", "meetingWith": "Ahmed",
+                "slotDate": "2022-05-03", "slotTime": "19:05", "duration": "30" })
+            .then((res) => {
+                const body = res.body;
+                console.log(body)
+                expect(body).to.contain.property("error");
+                expect(body.error).to.equal('Ahmed is busy, Please select different time slot');
         
-    //             done();
-    //         })
-    //         .catch((err) => done(err));
-    // });
+                done();
+            })
+            .catch((err) => done(err));
+    });
 
 })
